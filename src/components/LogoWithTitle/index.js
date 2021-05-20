3f65a911c5c0b4e7b7585fe947f9b615e1c5b9b5
kevin <incredibledev.sec@gmail.com>
2021-05-20 04:56:19 -0400

    [ infra ] : bootstrapping with create-react-app * implementing the theme * implementing the styling framwork as material ui * building the granular components which can be used in global scope like button, inputbox, select, image components, hooks etc * updating the metainfo * implementing the dialog and notistack components

--- src/components/LogoWithTitle/index.js
+++ src/components/LogoWithTitle/index.js
@@ -34,11 +34,9 @@ const LogoWithTitle = ({ setOpen, history, logoWidth, logoHeight, titleVariant, 
   return (
     <div className={clsx(classes.root, className)}>
       <CircleButton style={{ display: 'flex', backgroundColor: '#292C40' }} onClick={onClickHander} icon={<Logo className={classes.logo} width={logoWidth} height={logoHeight} />} />
-      {/* <Hidden mdDown implementation='css' className={classes.height}> */}
       <Typography color='textPrimary' variant={titleVariant}>
         Althash
       </Typography>
-      {/* </Hidden> */}
     </div>
   );
 };
